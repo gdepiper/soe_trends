f4b21124b873b9e54fc18687ad2cadd2c641721a
perretti <ctperretti@gmail.com>
2018-01-08 20:02:21 -0500

    Added GAM, GLS and plotting.

--- Time_series_trend_analysis_simulations_v2.R
+++ Time_series_trend_analysis_simulations_v2.R
@@ -3,6 +3,14 @@
 #December 14, 2017
 #10, 20, 30 years
 #AR(1) ARIMA(1,0,1), linear trend, quadratic trend, no trend
+
+# Load required libraries
+library(tidyr)
+library(ggplot2)
+
+# Load functions to do model selection of GLS and GAM
+source("model_compare.R")
+
 PKG <-c("Kendall",'zyp')
 
 for (p in PKG) {
@@ -13,68 +21,78 @@ for (p in PKG) {
 
 set.seed(436)
 
-n = 10 #number of simulations
+n <- 100 #number of simulations
 x <- 30 #number of periods
 
-LTREND1 <- 0.4232083+(0.004941981*c(1:x)) #Linear trend
-LTREND2 <- 0.3503154+(0.001059701*c(1:x))
-LTREND3 <- 0.4961013+(0.00882426*c(1:x))
-AR1 <- list(ar = 0.4354565)
-AR2<- list(ar = 0.8)
+LTRENDweak <- 0.42 + (0.00106 * c(1:x))
+LTRENDmedium <- 0.42 + (0.00494 * c(1:x)) #Linear trend
+LTRENDstrong <- 0.42 + (0.00882 * c(1:x))
+ARmedium <- list(ar = 0.435)
+ARstrong <- list(ar = 0.8)
 NOAR <- list()
 
 #Adding placeholders for simulated data
-LINEAR1_AR1 <- NULL 
-LINEAR1_AR2 <- NULL 
-LINEAR2_AR1 <- NULL 
-LINEAR2_AR2 <- NULL
-LINEAR3_AR1 <- NULL 
-LINEAR3_AR2 <- NULL 
+LINEARweak_ARmedium <- NULL 
+LINEARweak_ARstrong <- NULL 
+LINEARmedium_ARmedium <- NULL 
+LINEARmedium_ARstrong <- NULL
+LINEARstrong_ARmedium <- NULL 
+LINEARstrong_ARstrong <- NULL 
 
-NOTREND_AR1 <- NULL
-NOTREND_AR2 <- NULL
+NOTREND_ARmedium <- NULL
+NOTREND_ARstrong <- NULL
 NOTREND_NOAR <- NULL
 
-LINEAR1_NOAR <- NULL
-LINEAR2_NOAR <- NULL
-LINEAR3_NOAR <- NULL
+LINEARweak_NOAR <- NULL
+LINEARmedium_NOAR <- NULL
+LINEARstrong_NOAR <- NULL
 
-NOTREND_AR1_RESULTS <- NULL
+NOTREND_ARmedium_RESULTS <- NULL
 NOTREND_NOAR_RESULTS <- NULL
-NOTREND_AR2_RESULTS <- NULL
-LINEAR1_AR1_RESULTS <- NULL
-LINEAR1_AR2_RESULTS <- NULL
-LINEAR2_AR1_RESULTS <- NULL
-LINEAR2_AR2_RESULTS <- NULL
-LINEAR3_AR1_RESULTS <- NULL
-LINEAR3_AR2_RESULTS <- NULL
-LINEAR1_NOAR_RESULTS <- NULL
-LINEAR2_NOAR_RESULTS <- NULL
-LINEAR3_NOAR_RESULTS <- NULL
+NOTREND_ARstrong_RESULTS <- NULL
+LINEARweak_ARmedium_RESULTS <- NULL
+LINEARweak_ARstrong_RESULTS <- NULL
+LINEARmedium_ARmedium_RESULTS <- NULL
+LINEARmedium_ARstrong_RESULTS <- NULL
+LINEARstrong_ARmedium_RESULTS <- NULL
+LINEARstrong_ARstrong_RESULTS <- NULL
+LINEARweak_NOAR_RESULTS <- NULL
+LINEARmedium_NOAR_RESULTS <- NULL
+LINEARstrong_NOAR_RESULTS <- NULL
 
 #initializing simulations
 for (i in 1:n) {
 #Generating ar(1) simulations
-  for (k in c('AR1','AR2','NOAR')){
+  for (k in c('ARmedium','ARstrong','NOAR')){
 TEMP1 <- arima.sim(get(k),n=x,rand.gen=rnorm)
-LTEMP1 <- TEMP1+LTREND1
-LTEMP2 <- TEMP1+LTREND2
-LTEMP3 <- TEMP1+LTREND3
-assign(paste0('LINEAR1_',k,sep=""),rbind(get(paste0('LINEAR1_',k,sep="")),LTEMP1))
-assign(paste0('LINEAR2_',k,sep=""),rbind(get(paste0('LINEAR2_',k,sep="")),LTEMP2))
-assign(paste0('LINEAR3_',k,sep=""),rbind(get(paste0('LINEAR3_',k,sep="")),LTEMP3))
+LTEMP1 <- TEMP1 + LTRENDweak
+LTEMP2 <- TEMP1 + LTRENDmedium
+LTEMP3 <- TEMP1 + LTRENDstrong
+assign(paste0('LINEARweak_',k,sep=""),rbind(get(paste0('LINEARweak_',k,sep="")),LTEMP1))
+assign(paste0('LINEARmedium_',k,sep=""),rbind(get(paste0('LINEARmedium_',k,sep="")),LTEMP2))
+assign(paste0('LINEARstrong_',k,sep=""),rbind(get(paste0('LINEARstrong_',k,sep="")),LTEMP3))
 
 assign(paste0('NOTREND_',k, sep=""),rbind(get(paste0('NOTREND_',k, sep="")), TEMP1))
 
 for (y in c('TEMP1','LTEMP1','LTEMP2','LTEMP3')){
+  # Print counter
+  
   #Testing 30 year series with prewhitening Mann-Kendall technique
-TEMP_TEST1 <- zyp.trend.vector(get(y),method='yuepilon')
+  TEMP_TEST1 <- zyp.trend.vector(get(y),method='yuepilon')
   TEMP_P1 <- unlist(TEMP_TEST1[6])
   TEMP_TREND1 <- unlist(TEMP_TEST1[3])
   T_1 <- cbind(TEMP_P1,TEMP_TREND1)
   #30 year standard Mann-Kendall
-TEMP_TEST12 <- MannKendall(get(y))
-T_2 <- as.double(unlist(TEMP_TEST12[2]))
+  TEMP_TEST12 <- MannKendall(get(y))
+  T_2 <- as.double(unlist(TEMP_TEST12[2]))
+  # 30-year linear model
+  TEMP_lm <- fit_lm(dat = data.frame(series = get(y) %>% as.numeric,
+                    time = 1:length(get(y))))
+  T_lm <- TEMP_lm$pval
+  # 30-year gam
+  TEMP_gam <- fit_gam(dat = data.frame(series = get(y) %>% as.numeric,
+                                       time = 1:length(get(y))))
+  T_gam <- TEMP_gam$pval
 
 for (j in c(10,20)) {
   #Testind 20 & 10 year series with prewhitening Mann-Kendall technique
@@ -86,16 +104,94 @@ for (j in c(10,20)) {
   TEMP_TEST22 <- MannKendall(get(y)[j:x])
   TEMP_P22 <- as.double(unlist(TEMP_TEST22[2]))
   T_2 <- cbind(T_2,TEMP_P22)
+  # 20-year and 10-year linear model
+  TEMP_lm <- fit_lm(dat = data.frame(series = get(y)[j:x] %>% as.numeric,
+                                     time = 1:length(get(y)[j:x])))
+  T_lm <- cbind(T_lm, TEMP_lm$pval)
+  # 20-year and 10-year gam
+  TEMP_gam <- fit_gam(dat = data.frame(series = get(y)[j:x] %>% as.numeric,
+                                       time = 1:length(get(y)[j:x])))
+  T_gam <- cbind(T_gam, TEMP_gam$pval)
 }
-  colnames(T_1) <- c('p_Val30_pw','Slope30_pw','p_Val20_pw','Slope20_pw','p_Val10_pw','Slope_10_pw')
-  colnames(T_2) <- c('p_Val30_mk','p_Val20_mk','p_Val10_mk')
+  colnames(T_1) <- c('p_30_pw','Slope30_pw','p_20_pw',
+                     'Slope20_pw','p_10_pw','Slope10_pw')
+  colnames(T_2) <- c('p_30_mk','p_20_mk','p_10_mk')
+  colnames(T_lm) <- c('p_30_gls','p_20_gls','p_10_gls')
+  colnames(T_gam) <- c('p_30_gam','p_20_gam','p_10_gam')
   
-if (y=='TEMP1' & k!='NOAR') {assign(paste0('NOTREND_',k,'_RESULTS',sep=""),rbind(get(paste0('NOTREND_',k,'_RESULTS',sep="")),cbind(T_1,T_2)))}
-  else if (y=='TEMP1' & k=='NOAR') {assign(paste0('NOTREND_',k,'_RESULTS',sep=""),rbind(get(paste0('NOTREND_',k,'_RESULTS',sep="")),cbind(T_1,T_2)))}
-  else if (y=='LTEMP1') {assign(paste0('LINEAR1_',k,'_RESULTS',sep=""),rbind(get(paste0('LINEAR1_',k,'_RESULTS',sep="")),cbind(T_1,T_2)))}
-  else if (y=='LTEMP2') {assign(paste0('LINEAR2_',k,'_RESULTS',sep=""),rbind(get(paste0('LINEAR2_',k,'_RESULTS',sep="")),cbind(T_1,T_2)))}
-  else if (y=='LTEMP3') {assign(paste0('LINEAR3_',k,'_RESULTS',sep=""),rbind(get(paste0('LINEAR3_',k,'_RESULTS',sep="")),cbind(T_1,T_2)))}
+if (y=='TEMP1' & k!='NOAR') {assign(paste0('NOTREND_',k,'_RESULTS',sep=""),
+                                    rbind(get(paste0('NOTREND_',k,'_RESULTS',sep="")),
+                                          cbind(T_1,T_2, T_lm, T_gam)))
+  } else if (y=='TEMP1' & k=='NOAR') {assign(paste0('NOTREND_',k,'_RESULTS',sep=""),
+                                           rbind(get(paste0('NOTREND_',k,'_RESULTS',sep="")),
+                                                 cbind(T_1,T_2, T_lm, T_gam)))
+  } else if (y=='LTEMP1') {assign(paste0('LINEARweak_',k,'_RESULTS',sep=""),
+                                rbind(get(paste0('LINEARweak_',k,'_RESULTS',sep="")),
+                                      cbind(T_1,T_2, T_lm, T_gam)))
+  } else if (y=='LTEMP2') {assign(paste0('LINEARmedium_',k,'_RESULTS',sep=""),
+                                rbind(get(paste0('LINEARmedium_',k,'_RESULTS',sep="")),
+                                      cbind(T_1,T_2, T_lm, T_gam)))
+  } else if (y=='LTEMP3') {assign(paste0('LINEARstrong_',k,'_RESULTS',sep=""),
+                                rbind(get(paste0('LINEARstrong_',k,'_RESULTS',sep="")),
+                                      cbind(T_1,T_2, T_lm, T_gam)))}
   }
 #rm(TEMP1,LTEMP1,QTEMP1)
 }
 }
+
+#convert Monte Carlo simulation output (p values and slopes)
+# from wide to long format for plotting
+library(data.table)
+results <- c("NOTREND_ARmedium_RESULTS", "NOTREND_NOAR_RESULTS", "NOTREND_ARstrong_RESULTS",
+             "LINEARweak_ARmedium_RESULTS", "LINEARweak_ARstrong_RESULTS",
+             "LINEARmedium_ARmedium_RESULTS","LINEARmedium_ARstrong_RESULTS",
+             "LINEARstrong_ARmedium_RESULTS", "LINEARstrong_ARstrong_RESULTS",
+             "LINEARweak_NOAR_RESULTS","LINEARmedium_NOAR_RESULTS","LINEARstrong_NOAR_RESULTS")
+
+
+#wide to long function (Sean Lucey)
+w2l <- function(x, by, by.name = 'Time', value.name = 'Value'){
+  x.new <- copy(x)
+  var.names <- names(x)[which(names(x) != by)]
+  out <- c()
+  setnames(x.new, by, 'by')
+  for(i in 1:length(var.names)){
+    setnames(x.new, var.names[i], 'V1')
+    single.var <- x.new[, list(by, V1)]
+    single.var[, Var := var.names[i]]
+    out <- rbindlist(list(out, single.var))
+    setnames(x.new, 'V1', var.names[i])
+  }
+  setnames(x.new, 'by', by)
+  setnames(out, c('by', 'V1'), c(by.name, value.name))
+}
+
+#filter for p values
+p_result_mat <- list()
+for (i in 1:length(results)){
+  z <- get(results[i])
+  z <- cbind(z[,grepl("p_",colnames(get(results[i]))) == TRUE],seq(1,nrow(z),1))
+  z <- data.table(z)
+  z <- w2l(z, by = "V13", by.name = "replicate")
+  z <- cbind(z,rep(results[i],nrow(z)))
+  p_result_mat[[i]] <- z
+}
+
+# Final p dataframe
+p_results <- 
+  do.call(rbind, p_result_mat) %>%
+  as.data.frame() %>%
+  tidyr::separate(Var, 
+                  c("var", "timeseries length", "method"),
+                  "_")
+  
+
+
+# Box and whisker plot of each scenario
+ggplot(p_results, aes(color = method, y = Value, 
+                      x = `timeseries length`)) +
+  geom_boxplot() +
+  facet_wrap(~ V2, ncol = 3) +
+  ylab("p-value")
+
+
--- model_compare.R
+++ model_compare.R
@@ -4,31 +4,44 @@ library(MuMIn)
 
 ## Function to choose best linear model -------------------
 fit_lm <- function(dat) {
+  # Remove missing values first so that all models
+  # use the same number of observations (important for AIC)
+  dat <- dat %>% dplyr::filter(complete.cases(.))
+  
+  # Constant model (null model used to calculate 
+  # overall p-value)
+  constant_norm <-
+    nlme::gls(series ~ 1, 
+              data = dat)
+  
+  constant_ar1 <-
+    nlme::gls(series ~ 1,
+              data = dat,
+              correlation = nlme::corAR1(form = ~time))
+  
+  
+  
   # Linear model with normal error
   linear_norm <- 
-    nlme::gls(y ~ time, 
-              data = dat, 
-              na.action = na.omit)
+    nlme::gls(series ~ time, 
+              data = dat)
   
   # Linear model with AR1 error
   linear_ar1 <- 
-    nlme::gls(y ~ time, 
-              data = dat, 
-              na.action = na.omit,
+    nlme::gls(series ~ time, 
+              data = dat,
               correlation = nlme::corAR1(form = ~time))
   
   # Polynomial model with normal error
   dat$time2 <- dat$time^2
   poly_norm <- 
-    nlme::gls(y ~ time + time2, 
-              data = dat, 
-              na.action = na.omit)
+    nlme::gls(series ~ time + time2, 
+              data = dat)
   
   # Polynomial model with AR1 error
   poly_ar1 <- 
-    nlme::gls(y ~ time + time2, 
-              data = dat, 
-              na.action = na.omit,
+    nlme::gls(series ~ time + time2, 
+              data = dat,
               correlation = nlme::corAR1(form = ~time))
   
   # Calculate AICs for all models
@@ -43,8 +56,18 @@ fit_lm <- function(dat) {
                          AICc(linear_ar1)),
                coefs = rbind(coef(poly_norm),
                              coef(poly_ar1),
-                             c(coef(linear_norm),NA),
-                             c(coef(linear_ar1),NA)))
+                             c(coef(linear_norm), NA),
+                             c(coef(linear_ar1),  NA)),
+               # Calculate overall signifiance (need to use
+               # ML not REML for this)
+               pval = c(anova(update(constant_norm, method = "ML"), 
+                              update(poly_norm, method = "ML"))$`p-value`[2],
+                        anova(update(constant_ar1, method = "ML"), 
+                              update(poly_ar1, method = "ML"))$`p-value`[2],
+                        anova(update(constant_norm, method = "ML"), 
+                              update(linear_norm, method = "ML"))$`p-value`[2],
+                        anova(update(constant_ar1, method = "ML"), 
+                              update(linear_ar1, method = "ML"))$`p-value`[2]))
   
   best_lm <-
     df_aicc %>%
@@ -55,17 +78,30 @@ fit_lm <- function(dat) {
 
 ## Function to choose best GAM ----------------------------
 fit_gam <- function(dat) {
+    
+  # Remove missing values first so that all models
+  # use the same number of observations (important for AIC)
+  dat <- dat %>% dplyr::filter(complete.cases(.))
+  
+  # Break out if less than 15 data points (GAMM runs into numerical problems)
+  if (nrow(dat) < 15) {
+    best_gam <- 
+      data.frame(model = NA,
+                 aicc  = NA,
+                 coefs = NA,
+                 pval  = NA) 
+    return(best_gam)
+  }
+  
   # GAM with normal error
   gam_norm <- 
-    mgcv::gam(y ~ s(time), 
-              data = dat, 
-              na.action = na.omit)
+    mgcv::gam(series ~ s(time), 
+              data = dat)
   
   # GAM with AR1 error
   gam_ar1 <- 
-    mgcv::gamm(y ~ s(time), 
-               data = dat, 
-               na.action = na.omit,
+    mgcv::gamm(series ~ s(time), 
+               data = dat,
                correlation = corAR1(form = ~time))
   
   # Calculate AICs for all models
@@ -75,22 +111,25 @@ fit_gam <- function(dat) {
                aicc  = c(AICc(gam_norm),
                          AICc(gam_ar1)),
                coefs = rbind(coef(gam_norm),
-                             coef(gam_ar1$lme)))
+                             coef(gam_ar1$gam)),
+               pval  = c(summary(gam_norm)$s.table[, "p-value"],
+                         summary(gam_ar1$gam)$s.table[, "p-value"]))
   
   best_gam <-
     df_aicc %>%
     dplyr::filter(aicc == min(aicc))
+  
   
   return(best_gam)
 }
 
 
 ## Demo the functions -------------------------------------
-dat <- data.frame(y = 0:49 + rnorm(50),
-                  time = 0:49)
-
-plot(y ~ time, dat, type = "b")
-
-fit_lm(dat = dat)
-
-fit_gam(dat = dat)
+# dat <- data.frame(series = 0:49 + rnorm(50),
+#                   time = 0:49)
+# 
+# plot(series ~ time, dat, type = "b")
+# 
+# fit_lm(dat = dat)
+# 
+# fit_gam(dat = dat)
